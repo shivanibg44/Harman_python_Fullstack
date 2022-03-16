c3c173c4ee164ddf508e9a8d21987a15288f8e1e
shivanibg44 <99085505+shivanibg44@users.noreply.github.com>
2022-03-16 23:55:45 +0530

    Delete Java/Day 9/Day10 directory

--- Java/Day 9/Day10/src/error prog.java
+++
@@ -1,9 +0,0 @@
-public class Error {
-    public static void main(String[] args) {
-        int a = 10;
-        float b = 88;
-        int c = (int) ((++a) + b);
-        System.out.println(c);
-
-    }
-}
