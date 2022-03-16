e818e15458687aa2ae0cfaf3af4f9a5a5d715eb8
shivanibg44 <99085505+shivanibg44@users.noreply.github.com>
2022-03-16 23:57:52 +0530

    Delete Java/day 10 directory

--- Java/day 10/src/Convert.java
+++
@@ -1,9 +0,0 @@
-public class Convert {
-    public static void main(String[] args) {
-        float Fahrenheit, Celsius;
-        Fahrenheit = 43;
-        Celsius = ((Fahrenheit-32)*5)/9;
-        System.out.println("Temoerature in celsius is:"+Celsius);
-
-    }
-}
